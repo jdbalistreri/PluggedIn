e912423eeb3e1aa60b23ab092ca9382132defef0
Joe Balistreri <Jdbalistreri@gmail.com>
2015-03-10 15:33:41 -0400

    fix db seeds to use Date class

--- app/assets/javascripts/views/user_show.js
+++ app/assets/javascripts/views/user_show.js
@@ -2,6 +2,10 @@ ExtinctIn.Views.UserShow = Backbone.CompositeView.extend({
 
   template: JST["users/show"],
 
+  initialize: function () {
+    this.listenTo(this.model, "sync", this.render)
+  },
+
   render: function () {
     var content = this.template({user: this.model});
     this.$el.html(content);
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,12 @@
 user1 = User.create!(email: "joe", password: "joejoe",
   fname: "Joe", lname: "Bali", tagline: "ain't life grand?",
   location: "Milwaukee, WI", industry: "Computer Software",
-  date_of_birth: "07/24/1991", summary: "This is my summary")
+  date_of_birth: Date.new(1991, 7, 24), summary: "This is my summary")
 
 user2 = User.create!(email: "joe2", password: "joejoe",
   fname: "John", lname: "Masters", tagline: "life ain't grand",
   location: "Cleveland, OH", industry: "Law",
-  date_of_birth: "01/21/1950", summary: "Summary 2")
+  date_of_birth: Date.new(1950, 1, 23), summary: "Summary 2")
 
 user3 = User.create!(email: "joe3", password: "joejoe")
 user4 = User.create!(email: "joe4", password: "joejoe")

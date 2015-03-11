3ba8299346c911c61aec319b2aa4992a77a27fcf
Joe Balistreri <jdbalistreri@gmail.com>
2015-03-11 18:12:44 -0400

    add experience collection sort

--- app/assets/javascripts/collections/jobs.js
+++ app/assets/javascripts/collections/jobs.js
@@ -4,6 +4,37 @@ ExtinctIn.Collections.Jobs = Backbone.Collection.extend({
 
   initialize: function (models, options) {
     this.user = options.user;
-  }
+  },
+
+  comparator: function (modelA, modelB) {
+    // debugger
+    if (modelA.get("end_year") === null) {
+      if (modelB.get("end_year") === null) {
+        return this.startDateSort(modelA, modelB);
+      } else {
+        return -1;
+      }
+    } else if (modelB.get("end_year") === null) {
+      return 1;
+    } else {
+      return this.startDateSort(modelA, modelB);
+    }
+  },
+
+  startDateSort: function (modelA, modelB) {
+    if (modelA.get("start_year") === modelB.get("start_year")) {
+      if (modelA.get("start_month") === modelB.get("start_month")) {
+        return 0;
+      } else if (modelA.get("start_month") < modelB.get("start_month")) {
+        return 1;
+      } else {
+        return -1;
+      }
+    } else if (modelA.get("start_year") < modelB.get("start_year")) {
+      return 1;
+    } else {
+      return -1;
+    }
+  },
 
 })

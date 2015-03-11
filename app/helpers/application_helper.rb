51d150d9d7249fd82c4a4515a6d9d72d0e63cbac
Joe Balistreri <jdbalistreri@gmail.com>
2015-03-11 12:03:24 -0400

    fix auth token bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def auth_token
     <<-HTML.html_safe
-      <input type="hidden" name="authenticity_token" value="<%=form_authenticity_token%>">
+      <input type="hidden" name="authenticity_token" value="#{form_authenticity_token}">
     HTML
   end
 

71734417e5a2c2a539280aec5c0a8d361a4a47e0
Joe Balistreri <jdbalistreri@gmail.com>
2015-03-16 11:07:08 -0400

    remove n+1 queries in search

--- app/controllers/api/static_controller.rb
+++ app/controllers/api/static_controller.rb
@@ -3,15 +3,16 @@ class Api::StaticController < ApplicationController
   def search
     found_experiences = PgSearch.multisearch(params[:query])
                         .where(searchable_type: "Experience")
-                        .includes(searchable: :user)
+                        .includes(searchable: {user: :connections})
                         .map(&:searchable)
 
     found_users =       PgSearch.multisearch(params[:query])
                         .where(searchable_type: "User")
+                        .includes(searchable: :connections)
                         .map(&:searchable)
 
     @result_users = found_experiences.map(&:user).concat(found_users).uniq
-    @result_users = User.all if @result_users.empty?
+    @result_users = User.all.includes(:connections) if @result_users.empty?
   end
 
   def connections_search

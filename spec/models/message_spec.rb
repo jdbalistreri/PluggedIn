0265f9c2b9ff22c30a11274d8ea5b4871a44004b
Joe Balistreri <jdbalistreri@gmail.com>
2015-03-17 10:36:53 -0400

    write basic rspec for message model

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,26 +2,38 @@ require 'rails_helper'
 
 
 describe Message, type: :model do
-  it "validates the presence of a subject and title" do
-    m = Message.new(subject: "subject", body: "body",
-                    sender_id: 1, receiver_id: 2)
+  let(:user1) { User.create!(email: "user1@example.com", password: "password",
+                              fname: "joe", lname: "bal")}
+  let(:user2) { User.create!(email: "user2@example.com", password: "password",
+                              fname: "joe", lname: "bal")}
 
+  it "validates the presence of a subject and title" do
+    m = user1.sent_messages.new(subject: "subject", body: "body",
+                    receiver_id: user2.id)
     expect(m.valid?).to be(true)
 
-    m2 = Message.new(body: "body",
-                    sender_id: 1, receiver_id: 2)
-
+    m2 = user1.sent_messages.new(body: "body", receiver_id: user2.id)
     expect(m2.valid?).to be(false)
 
-    m3 = Message.new(subject: "subject",
-                    sender_id: 1, receiver_id: 2)
-
+    m3 = user1.sent_messages.new(subject: "subject", receiver_id: user2.id)
     expect(m3.valid?).to be(false)
   end
 
+  it "validates the presence of a sender and a receiver" do
+    m = user1.sent_messages.new(subject: 's', body: 'b', receiver_id: user2.id)
+    expect(m.valid?).to be(true)
 
-  it "validates the presence of a sender and a receiver"
+    m2 = user1.sent_messages.new(subject: 's', body: 'b')
+    expect(m2.valid?).to be(false)
 
-  it "validates that a user cannot message herself"
+    m3 = Message.new(subject: 's', body: 'b', sender_id: user1.id)
+    expect(m2.valid?).to be(false)
+
+  end
+
+  it "validates that a user cannot message herself" do
+    m = user1.sent_messages.new(subject: 's', body: 'b', receiver_id: user1.id)
+    expect(m.valid?).to be(false)
+  end
 
 end

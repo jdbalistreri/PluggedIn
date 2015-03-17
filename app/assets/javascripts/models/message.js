2455a3d3e2a35baa8ac7bb6206efa9a2a7aeca82
Joe Balistreri <jdbalistreri@gmail.com>
2015-03-17 11:32:15 -0400

    add parse method to message model

--- app/assets/javascripts/models/message.js
+++ app/assets/javascripts/models/message.js
@@ -2,4 +2,20 @@ ExtinctIn.Models.Message = Backbone.Model.extend({
 
   urlRoot: "api/messages",
 
+  parse: function (response) {
+    if (response.user) {
+      this.user(response.user);
+      delete response.user;
+    }
+
+    return response
+  },
+
+  user: function (attrs) {
+    if (attrs) {
+      this._user = new ExtinctIn.Models.User(attrs, {parse: true});
+    }
+    return this._user
+  },
+
 })

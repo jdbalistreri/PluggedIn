a8380445f81935c3388d1cb350452de1ed83845a
Joe Balistreri <jdbalistreri@gmail.com>
2015-05-03 11:54:22 -0400

    complete tests for sessions controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -11,6 +11,7 @@ describe SessionsController do
         post :create, user: { email: user.email, password: "invalid" }
 
         expect(response).to render_template(:new)
+        expect(session[:token]).to be(nil)
       end
     end
 
@@ -19,15 +20,45 @@ describe SessionsController do
         user = create(:user, password: "password")
 
         post :create, user: { email: user.email, password: "password" }
-        expect(response). to redirect_to '/'
+        expect(response).to redirect_to '/'
+        expect(session[:token]).to_not be(nil)
         expect(controller.current_user).to eq(user)
       end
     end
 
   end
 
-  describe "#destroy"
+  describe "#destroy" do
+    let(:user) { create(:user, password: "password") }
 
-  describe "#new"
+    before(:each) do
+      post :create, user: { email: user.email, password: "password" }
+    end
+
+    it "clears the session token" do
+      delete :destroy
+      expect(session[:token]).to be(nil)
+    end
+
+    it "resets the user's session token attribute" do
+      current_user = @controller.current_user
+      old_token = current_user.session_token
+      delete :destroy
+      expect(current_user.session_token).to_not eq(old_token)
+    end
+
+    it "redirects to the new session url" do
+      delete :destroy
+      expect(response).to redirect_to '/session/new'
+    end
+
+  end
+
+  describe "#new" do
+    it 'renders the new session view' do
+      get :new
+      assert_template :new
+    end
+  end
 
 end
